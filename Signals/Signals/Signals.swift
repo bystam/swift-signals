4c05014ce105fbcfad77cb11ca2c5a03ed0b7b5b
Fredrik Bystam <fredrik.bystam@kry.se>
2018-11-14 22:16:24 +0100

    implement thread safety for source

--- Signals/Signals/Signals.swift
+++ Signals/Signals/Signals.swift
@@ -37,34 +37,47 @@ final class Source<Element>: Signal<Element> {
     private var order: [UUID] = []
     private var handlers: [UUID: Handler] = [:]
 
+    private let mutex = DispatchQueue(label: "Source.mutex", attributes: .concurrent)
+
     init(options: Set<SourceOption> = []) {
         self.options = options
     }
 
     override func listen(_ handler: @escaping (Element) -> Void) -> SignalToken {
         let id = UUID()
-        order.append(id)
-        handlers[id] = handler
+        mutate(id: id, set: handler)
 
         return SourceToken { [weak self] in
-            self?.removeListener(withID: id)
+            self?.mutate(id: id, set: nil)
         }
     }
 
     func publish(_ element: Element) {
-        if options.contains(.publishOnlyToLatestListener) {
-            order.last
-                .flatMap { handlers[$0] }?(element)
-        } else {
-            order.reversed()
-                .compactMap { handlers[$0] }
-                .forEach { $0(element) }
+        var handlers: [Handler] = []
+        mutex.sync {
+            if self.options.contains(.publishOnlyToLatestListener) {
+                if let handler = self.order.last.flatMap({ self.handlers[$0] }) {
+                    handlers.append(handler)
+                }
+            } else {
+                handlers = self.order.reversed()
+                    .compactMap { self.handlers[$0] }
+//                    .forEach { $0(element) }
+            }
         }
+        handlers.forEach { $0(element) }
     }
 
-    private func removeListener(withID id: UUID) {
-        handlers[id] = nil
-        order.removeAll(where: { $0 == id })
+    private func mutate(id: UUID, set handler: Handler?) {
+        mutex.async(flags: .barrier) {
+            if let handler = handler {
+                self.order.append(id)
+                self.handlers[id] = handler
+            } else {
+                self.handlers[id] = nil
+                self.order.removeAll(where: { $0 == id })
+            }
+        }
     }
 }
 
@@ -153,12 +166,14 @@ protocol SignalToken: AnyObject {}
 
 extension SignalToken {
     func bindLifetime(to bag: SignalTokenBag) {
-        bag.tokens.append(self)
+        bag.append(self)
     }
 }
 
 final class SignalTokenBag: SignalToken {
-    fileprivate var tokens: [SignalToken]
+
+    private let mutex = DispatchQueue(label: "Source.mutex", attributes: .concurrent)
+    private var tokens: [SignalToken]
 
     init() {
         self.tokens = []
@@ -166,6 +181,12 @@ final class SignalTokenBag: SignalToken {
 
     init(tokens: [SignalToken]) {
         self.tokens = tokens
+    }
+
+    fileprivate func append(_ token: SignalToken) {
+        mutex.async(flags: .barrier) {
+            self.tokens.append(token)
+        }
     }
 }
 
