9822de2171388abd9f8295bb5217b0503dcacf28
Fredrik Bystam <fredrik.bystam@kry.se>
2018-11-19 21:07:26 +0100

    implement thread safety for combine and zip

--- Signals/Signals/Signal+Combine.swift
+++ Signals/Signals/Signal+Combine.swift
@@ -8,42 +8,42 @@ extension Signal {
 
     static func combine<A, B>(_ a: Signal<A>, _ b: Signal<B>, with transform: @escaping (A, B) -> Element) -> Signal<Element> {
         let upstreams = [ a.typeErase(), b.typeErase() ]
-        return Combinator<Element>(upstreams: upstreams, type: .combine, transform: { values in
+        return Combinator<Element>(upstreams: upstreams, resetOnGenerate: false, transform: { values in
             return transform(values[0] as! A, values[1] as! B)
         })
     }
 
     static func combine<A, B, C>(_ a: Signal<A>, _ b: Signal<B>, _ c: Signal<C>, with transform: @escaping (A, B, C) -> Element) -> Signal<Element> {
         let upstreams = [ a.typeErase(), b.typeErase(), c.typeErase() ]
-        return Combinator<Element>(upstreams: upstreams, type: .combine, transform: { values in
+        return Combinator<Element>(upstreams: upstreams, resetOnGenerate: false, transform: { values in
             return transform(values[0] as! A, values[1] as! B, values[2] as! C)
         })
     }
 
     static func combine<A, B, C, D>(_ a: Signal<A>, _ b: Signal<B>, _ c: Signal<C>, _ d: Signal<D>, with transform: @escaping (A, B, C, D) -> Element) -> Signal<Element> {
         let upstreams = [ a.typeErase(), b.typeErase(), c.typeErase(), d.typeErase() ]
-        return Combinator<Element>(upstreams: upstreams, type: .combine, transform: { values in
+        return Combinator<Element>(upstreams: upstreams, resetOnGenerate: false, transform: { values in
             return transform(values[0] as! A, values[1] as! B, values[2] as! C, values[3] as! D)
         })
     }
 
     static func zip<A, B>(_ a: Signal<A>, _ b: Signal<B>, with transform: @escaping (A, B) -> Element) -> Signal<Element> {
         let upstreams = [ a.typeErase(), b.typeErase() ]
-        return Combinator<Element>(upstreams: upstreams, type: .zip, transform: { values in
+        return Combinator<Element>(upstreams: upstreams, resetOnGenerate: true, transform: { values in
             return transform(values[0] as! A, values[1] as! B)
         })
     }
 
     static func zip<A, B, C>(_ a: Signal<A>, _ b: Signal<B>, _ c: Signal<C>, with transform: @escaping (A, B, C) -> Element) -> Signal<Element> {
         let upstreams = [ a.typeErase(), b.typeErase(), c.typeErase() ]
-        return Combinator<Element>(upstreams: upstreams, type: .zip, transform: { values in
+        return Combinator<Element>(upstreams: upstreams, resetOnGenerate: true, transform: { values in
             return transform(values[0] as! A, values[1] as! B, values[2] as! C)
         })
     }
 
     static func zip<A, B, C, D>(_ a: Signal<A>, _ b: Signal<B>, _ c: Signal<C>, _ d: Signal<D>, with transform: @escaping (A, B, C, D) -> Element) -> Signal<Element> {
         let upstreams = [ a.typeErase(), b.typeErase(), c.typeErase(), d.typeErase() ]
-        return Combinator<Element>(upstreams: upstreams, type: .zip, transform: { values in
+        return Combinator<Element>(upstreams: upstreams, resetOnGenerate: true, transform: { values in
             return transform(values[0] as! A, values[1] as! B, values[2] as! C, values[3] as! D)
         })
     }
@@ -60,23 +60,25 @@ private final class Combinator<Element>: Signal<Element> {
     }
 
     private let upstreams: [Signal<Any>]
-    private let type: FrequencyType
+    private let resetOnGenerate: Bool
     private let transform: ([Any]) -> Element
 
-    private var preListeningElements: [Any?]
+    private let preListeningElements: Elements
+//    private var preListeningElements: [Any?]
     private var upstreamToken: SignalToken?
 
-    init(upstreams: [Signal<Any>], type: FrequencyType, transform: @escaping ([Any]) -> Element) {
+    init(upstreams: [Signal<Any>], resetOnGenerate: Bool, transform: @escaping ([Any]) -> Element) {
         self.upstreams = upstreams
-        self.type = type
+        self.resetOnGenerate = resetOnGenerate
         self.transform = transform
-        self.preListeningElements = Array(repeating: nil, count: upstreams.count)
+        self.preListeningElements = Elements(array: Array(repeating: nil, count: upstreams.count))
 
         super.init()
 
         let tokens = upstreams.enumerated().map { index, upstream in
             return upstream.listen(with: self, { this, element in
-                _ = this.generateValueIfFilled(inserting: element, at: index, in: &this.preListeningElements)
+                _ = this.preListeningElements.insert(value: element, at: index, resetOnGenerate: resetOnGenerate)
+//                _ = this.generateValueIfFilled(inserting: element, at: index, in: &this.preListeningElements)
             })
         }
 
@@ -84,27 +86,57 @@ private final class Combinator<Element>: Signal<Element> {
     }
 
     override func listen(_ handler: @escaping (Element) -> Void) -> SignalToken {
-        var elements: [Any?] = preListeningElements
+        let elements = preListeningElements.clone()
 
         let tokens = upstreams.enumerated().map { index, upstream in
             return upstream.listen(with: self, { this, element in
-                guard let element = this.generateValueIfFilled(inserting: element, at: index, in: &elements) else { return }
-                handler(element)
+                let generated = elements.insert(value: element, at: index, resetOnGenerate: this.resetOnGenerate)
+                generated.map(this.transform).map(handler)
             })
         }
 
         return SignalTokenBag(tokens: tokens)
     }
+//
+//    private func generateValueIfFilled(inserting value: Any, at index: Int, in elements: inout [Any?]) -> Element? {
+//        elements[index] = value
+//        let existingElements = elements.filter { $0 != nil }.map { $0! }
+//        if existingElements.count == upstreams.count {
+//            if type == .zip {
+//                elements = Array(repeating: nil, count: upstreams.count)
+//            }
+//            return transform(existingElements)
+//        }
+//        return nil
+//    }
+}
 
-    private func generateValueIfFilled(inserting value: Any, at index: Int, in elements: inout [Any?]) -> Element? {
-        elements[index] = value
-        let existingElements = elements.filter { $0 != nil }.map { $0! }
-        if existingElements.count == upstreams.count {
-            if type == .zip {
-                elements = Array(repeating: nil, count: upstreams.count)
+private final class Elements {
+
+    private var array: [Any?]
+    private let mutex = DispatchQueue(label: "Combinator.Elements.mutex", attributes: .concurrent)
+
+    init(array: [Any?]) {
+        self.array = array
+    }
+
+    func insert(value: Any, at index: Int, resetOnGenerate: Bool) -> [Any]? {
+        return mutex.sync(flags: .barrier) {
+            array[index] = value
+            let existingElements = array.filter { $0 != nil }.map { $0! }
+            if existingElements.count == array.count {
+                if resetOnGenerate {
+                    array = Array(repeating: nil, count: array.count)
+                }
+                return existingElements
             }
-            return transform(existingElements)
+            return nil
         }
-        return nil
+    }
+
+    func clone() -> Elements {
+        return mutex.sync(flags: .barrier) {
+            return Elements(array: array)
+        }
     }
 }
--- SignalsTests/SignalsConcurrencyTests.swift
+++ SignalsTests/SignalsConcurrencyTests.swift
@@ -74,4 +74,80 @@ class SignalsConcurrencyTests: XCTestCase {
 
         XCTAssertEqual(values.count, 1000)
     }
+
+    func testCombine_threadSafety1() {
+        let source1 = Source<Int>()
+        let source2 = Source<Int>()
+        let source3 = Source<Int>()
+        let signal = Source<Int>.combine(source1, source2, source3) { a, b, c -> Int in
+            return max(a, b, c)
+        }
+        var values: [Int] = []
+
+        let exp = expectation(description: "lol")
+        exp.expectedFulfillmentCount = 1001
+
+        signal.listen { int in
+            DispatchQueue.main.async {
+                values.append(int)
+                exp.fulfill()
+            }
+        }.bindLifetime(to: self.bag)
+
+        source2.publish(-1)
+        source1.publish(-1)
+        source3.publish(-1)
+
+        DispatchQueue.concurrentPerform(iterations: 1000) { i in
+            signal.listen { _ in
+            }.bindLifetime(to: self.bag)
+
+            switch Int.random(in: 0...2) {
+            case 0: source1.publish(i)
+            case 1: source2.publish(i)
+            case 2: source3.publish(i)
+            default: fatalError()
+            }
+        }
+
+        wait(for: [exp], timeout: 5.0)
+        XCTAssertEqual(values.count, 1001)
+    }
+
+
+    func testZip_threadSafety1() {
+        let source1 = Source<Int>()
+        let source2 = Source<Int>()
+        let source3 = Source<Int>()
+        let source4 = Source<Int>()
+        let signal = Source<Int>.zip(source1, source2, source3, source4) { a, b, c, d -> Int in
+            return max(a, b, c, d)
+        }
+        var values: [Int] = []
+
+        let exp = expectation(description: "lol")
+        exp.expectedFulfillmentCount = 1000
+
+        signal.listen { int in
+            DispatchQueue.main.async {
+                values.append(int)
+                exp.fulfill()
+            }
+        }.bindLifetime(to: self.bag)
+
+        (0..<1000).forEach { outerInt in
+            DispatchQueue.concurrentPerform(iterations: 4) { i in
+                switch i % 4 {
+                case 0: source1.publish(outerInt)
+                case 1: source2.publish(outerInt)
+                case 2: source3.publish(outerInt)
+                case 3: source4.publish(outerInt)
+                default: fatalError()
+                }
+            }
+        }
+
+        wait(for: [exp], timeout: 5.0)
+        XCTAssertEqual(values.count, 1000)
+    }
 }
